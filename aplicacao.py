33de75efc7cc4a290d7a8dfcd58917e1dc8c07a7
brendon6799 <61213371+brendon6799@users.noreply.github.com>
2020-04-29 22:06:18 -0300

    Add files via upload

--- aplicacao.py
+++ aplicacao.py
@@ -1,17 +1,3 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route('/home')
-def ola():
-    return '<h1>Olá mundo de novo!</h1>'
-
-@app.route('/')
-def pagina_principal():
-    return '<h1>Página Principal</h1>'
-
-@app.route('/')
-def outra_pagina():
-    return '<h1>Outra página</h1>'
-
-app.run(debug=True)
--- controller.py
+++ controller.py
@@ -2,22 +2,26 @@ from aplicacao import app
 
 from flask import render_template
 
+
 @app.route('/home')
 def ola():
     retorno = render_template(
             'index.html',
             title='Página Flask',
             outra='Novo Texto',
-            lista=['a','b','c']
+            lista=['a', 'b', 'c']
             )
     return retorno
+
 
 @app.route('/')
 def pagina_principal():
     return '<h1>Página Principal</h1><p>Olá</p>'
 
+
 @app.route('/outraPagina')
 def outra_pagina():
     return '<h1>Outra página</h1>'
+
 
 app.run(debug=True)
